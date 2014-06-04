8bc6b30dc023435c08c2f22fd3ded30d05ad7137
RYan <poloeded@hotmail.com>
2014-06-04 10:33:44 +0800

    add search cell

---
+++ mystock/class/SelectionViewController/SearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewCell.h
+//  mystock
+//
+//  Created by Ryan on 14-6-2.
+//  Copyright (c) 2014年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewCell : UITableViewCell
+
+@end
---
+++ mystock/class/SelectionViewController/SearchTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  SearchTableViewCell.m
+//  mystock
+//
+//  Created by Ryan on 14-6-2.
+//  Copyright (c) 2014年 Ryan. All rights reserved.
+//
+
+#import "SearchTableViewCell.h"
+
+@implementation SearchTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

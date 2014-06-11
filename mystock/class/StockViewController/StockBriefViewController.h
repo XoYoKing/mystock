55a879266cf10a2f40b625badb0131e40d0a37d3
RYan <poloeded@hotmail.com>
2014-06-11 17:28:15 +0800

    add stock brief

---
+++ mystock/class/StockViewController/StockBriefViewController.h
@@ -0,0 +1,13 @@
+//
+//  StockBriefViewController.h
+//  mystock
+//
+//  Created by Ryan on 14-6-11.
+//  Copyright (c) 2014年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StockBriefViewController : UIViewController
+
+@end
---
+++ mystock/class/StockViewController/SuggestDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuggestDetailViewController.h
+//  mystock
+//
+//  Created by Ryan on 14-6-11.
+//  Copyright (c) 2014年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuggestDetailViewController : UIViewController
+
+@end
